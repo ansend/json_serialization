343fabe072d85e28e99e7a7ac476922196104428
ansendong <ansenddong@gmail.com>
2021-05-20 10:43:44 +0800

    optimize code format

---
+++ compile.sh
@@ -0,0 +1,12 @@
+g++  -c  jsoncpp.cpp -I. -I./json
+
+g++  -c  jsoncpp.cpp -I. -I./json
+g++ jsoncpp.o main.cpp -I. -I./json -o tjson
+g++ -std=c++11 jsoncpp.o main.cpp -I. -I./json -o tjson
+
+g++ -std=c++11 jsoncpp.o template_test.cpp -I. -I./json -o templ
+
+g++ -std=c++11 jsoncpp.o template_test.cpp -I. -I./json -o templ
+
+g++ -E template_test.cpp
+
